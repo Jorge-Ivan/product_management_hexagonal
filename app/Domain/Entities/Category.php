da615dce492f3e00a05dabf8b3cd2df501ee4774
Jorge Ivan Carrillo Gonzalez <desarrollo18@domina.com.co>
2024-05-17 17:36:50 -0500

    Domain: Entities

---
+++ app/Domain/Entities/Category.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Domain\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+}
