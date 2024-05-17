9ca724e2a61b3ef47145d0057aaf552bdaaa8f7a
Jorge Ivan Carrillo Gonzalez <desarrollo18@domina.com.co>
2024-05-17 17:26:42 -0500

    Dominio: repositorios

---
+++ app/Domain/Repositories/CategoryRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Domain\Repositories;
+
+use App\Domain\Entities\Category;
+
+interface CategoryRepositoryInterface
+{
+    public function create(array $data): Category;
+    public function update(Category $category, array $data): bool;
+    public function findById(int $id): ?Category;
+    public function findAll(): array;
+}
---
+++ app/Domain/Repositories/ProductRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Domain\Repositories;
+
+use App\Domain\Entities\Product;
+
+interface ProductRepositoryInterface
+{
+    public function create(array $data): Product;
+    public function update(Product $product, array $data): bool;
+    public function findById(int $id): ?Product;
+    public function findAll(): array;
+    public function findByCategoryAndPrice(array $filters): array;
+}
