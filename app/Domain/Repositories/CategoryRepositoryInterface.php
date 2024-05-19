b8e8ddf48315e94c831ba967ced8e4e6095aa764
Jorge Ivan Carrillo Gonzalez <desarrollo18@domina.com.co>
2024-05-19 18:28:58 -0500

    Ajuste interfaces

--- app/Domain/Repositories/CategoryRepositoryInterface.php
+++ app/Domain/Repositories/CategoryRepositoryInterface.php
@@ -7,6 +7,7 @@ interface CategoryRepositoryInterface
 {
     public function create(array $data): Category;
     public function update(Category $category, array $data): bool;
+    public function delete(Category $category): bool;
     public function findById(int $id): ?Category;
     public function findAll(): array;
 }
--- app/Domain/Repositories/ProductRepositoryInterface.php
+++ app/Domain/Repositories/ProductRepositoryInterface.php
@@ -7,6 +7,7 @@ interface ProductRepositoryInterface
 {
     public function create(array $data): Product;
     public function update(Product $product, array $data): bool;
+    public function delete(Product $product): bool;
     public function findById(int $id): ?Product;
     public function findAll(): array;
     public function findByCategoryAndPrice(array $filters): array;
